390d7e44d849f912f1173b57c088366b6ee0a56a
g4rcez <allan.f.garcez@gmail.com>
2020-04-26 03:59:56 -0300

    project definition files

---
+++ src/global.d.ts
@@ -0,0 +1,27 @@
+import { Commands } from "./helpers/constants";
+
+export type ArgsPrompt =
+  | {
+      $mid: number;
+      fsPath: string;
+      external: string;
+      path: string;
+      scheme: string;
+    }
+  | undefined;
+
+export type FromPrompt = {
+  title: string;
+  value?: string;
+};
+
+export type CreateFile = {
+  path: string;
+  name: string;
+  extension: string;
+};
+
+export type SubscribeCommands = Array<{
+  name: Commands;
+  cmd: (args: ArgsPrompt) => Promise<unknown>;
+}>;
