f1acd4c1a396df2b2e656ea1f640e0fbb5c22040
g4rcez <allan.f.garcez@gmail.com>
2021-06-16 02:25:06 -0300

    refactor

--- src/commands/create-template-file.ts
+++ src/commands/create-template-file.ts
@@ -18,38 +18,39 @@ type CreateFileTemplate = {
   defaultValue: string;
   template?: FunctionTemplate;
 };
-export const createTemplateFile = (
-  ctx: vscode.ExtensionContext,
-  template: CreateFileTemplate
-) => async (args: ArgsPrompt) => {
-  const root = await getContextPath(ctx, args);
-  const name = await getFromPrompt({ title: "Filename", value: "" });
-  let extension = getConfigProperty<string>(template.propertyConfig);
-  if (extension === "") {
-    extension = await getFromPrompt({
-      title: template.promptTitle,
-      value: template.ext,
+export const createTemplateFile =
+  (ctx: vscode.ExtensionContext, template: CreateFileTemplate) =>
+  async (args: ArgsPrompt) => {
+    const root = await getContextPath(ctx, args);
+    const name =
+      (await getFromPrompt({ title: "Filename", value: "" })) ||
+      template.defaultValue;
+    let extension = getConfigProperty<string>(template.propertyConfig);
+    if (extension === "") {
+      extension = await getFromPrompt({
+        title: template.promptTitle,
+        value: template.ext,
+      });
+    }
+    const newFile = await createFile({
+      extension,
+      name,
+      path: root,
     });
-  }
-  const newFile = await createFile({
-    extension,
-    name,
-    path: root,
-  });
-  if (newFile !== null) {
-    await openFile(newFile);
-    if (template.template) {
-      await writeTemplate(
-        newFile,
-        template.template({ extension, filename: name })
-      );
-      await formatDocument();
-      await formatDocument();
-      const editor = vscode.window.activeTextEditor;
-      if (editor && editor.selection.isEmpty) {
-        const position = editor.selection.active;
-        position.with(0, 0);
+    if (newFile !== null) {
+      await openFile(newFile);
+      if (template.template) {
+        await writeTemplate(
+          newFile,
+          template.template({ extension, filename: name })
+        );
+        await formatDocument();
+        await formatDocument();
+        const editor = vscode.window.activeTextEditor;
+        if (editor && editor.selection.isEmpty) {
+          const position = editor.selection.active;
+          position.with(0, 0);
+        }
       }
     }
-  }
-};
+  };
